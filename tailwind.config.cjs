27aa358be89f2021a17ef90c5da2f8b16041997b
Atul Kumar Prajapati <prajapatiatul201@gmail.com>
2022-10-23 20:28:41 +0530

    dashboard and playlist

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,9 @@ module.exports = {
     extend: {
       colors: {
         "green":"#1db954",
-        "black":"#191414",
+        "black-base":"#121212",
+        "black-primary":"#191414",
+        "black-secondary":"#171818",
         "light-black":"#282828",
         "primary":"#ffffff",
         "secondary":"#b3b3b3",
@@ -16,5 +18,5 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/line-clamp'),],
 }
